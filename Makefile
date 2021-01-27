36d833be0a688aac4c662f036f4ab4eda3a89c3c
Ross Harrison <rtharrison86@gmail.com>
2021-01-26 19:13:54 -0600

    Create Contract for uploading file, assuming hash already exists

---
+++ Makefile
@@ -0,0 +1,15 @@
+.PHONY: migrate-local
+migrate-local:
+	truffle migrate --reset
+
+.PHONY: truffle-console
+truffle-console:
+	truffle console
+
+.PHONY: react-dev
+react-dev:
+	npx react-scripts start
+
+.PHONY: truffle-build
+truffle-build:
+	truffle build	
--- src/contracts/DStorage.sol
+++ src/contracts/DStorage.sol
@@ -2,36 +2,65 @@ pragma solidity ^0.5.0;
 
 contract DStorage {
   // Name
+  string public name = 'DStorage';
+  
   // Number of files
   // Mapping fileId=>Struct 
+  uint public fileCount = 0;
+  mapping (uint => File) public files;
 
-  // Struct
-
+  // File Struct
+  struct File {
+    uint fileId;
+    string fileHash;
+    uint fileSize;
+    string fileType;
+    string fileName;
+    string fileDescription;
+    uint uploadTime;
+    address payable uploader;
+  }
 
   // Event
+  event FileUploaded(
+    uint fileId,
+    string fileHash,
+    uint fileSize,
+    string fileType,
+    string fileName,
+    string fileDescription,
+    uint uploadTime,
+    address payable uploader
+  );
 
   constructor() public {
   }
 
   // Upload File function
-
+  function uploadFile(string memory _fileHash, uint _fileSize, string memory _fileType, string memory _fileName, string memory _fileDescription) public {
     // Make sure the file hash exists
-
+    require(bytes(_fileHash).length > 0);
     // Make sure file type exists
-
+    require(bytes(_fileType).length > 0);
     // Make sure file description exists
-
+    require(bytes(_fileDescription).length > 0);
     // Make sure file fileName exists
+    require(bytes(_fileName).length > 0);
 
     // Make sure uploader address exists
+    require(msg.sender != address(0x0));
 
     // Make sure file size is more than 0
+    require(_fileSize > 0);
 
 
     // Increment file id
-
+    fileCount += 1;
     // Add File to the contract
+    File memory newFile = File(fileCount, _fileHash, _fileSize, _fileType, _fileName, _fileDescription, now, msg.sender);
+    files[newFile.fileId] = newFile;
 
     // Trigger an event
-
+    emit FileUploaded(fileCount, _fileHash, _fileSize, _fileType, _fileName, _fileDescription, now, msg.sender);
+  }
 }
